6497eb00aa9d1a6598f7d3cd496a1b07776518dd
Amigo2808 <araj482367@gmail.com>
2024-07-18 07:22:01 +0530

    Implicit Intent

---
+++ 4_ImplicitIntent/app/src/main/java/com/amigo/implicitintent/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.amigo.implicitintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<Button>(R.id.btnChoosePicture).setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type="image/*"
+                startActivityForResult(it,0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val uri=data?.data
+        findViewById<ImageView>(R.id.ivImage).setImageURI(uri)
+    }
+}
