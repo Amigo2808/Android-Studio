d29d9f37e4285df3bc6ca9bdfc7ef62ce4e1f4ef
Amigo2808 <araj482367@gmail.com>
2024-07-18 07:22:42 +0530

    ToolBar Menu

---
+++ 5_ToolBarMenu/app/src/main/java/com/amigo/toolbarmenu/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.amigo.toolbarmenu
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.icExit->finish()
+            R.id.icProfile-> Toast.makeText(this, "Profile opened", Toast.LENGTH_SHORT).show()
+            R.id.icMessage-> Toast.makeText(this, "Message Opened", Toast.LENGTH_SHORT).show()
+            R.id.icHome-> Toast.makeText(this, "Home tab", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
